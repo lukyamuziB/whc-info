bd147ed9d50c685f28096f9c0fc43c87d62a0354
LukyamuziB <lukyamuzibenon@gmail.com>
2017-10-14 03:55:49 +0300

    made templates neater

--- app/main/views.py
+++ app/main/views.py
@@ -5,6 +5,20 @@ from . import main
 def index():
     return render_template('index.html')
 
+@main.route('/crowdfunding')
+def crowdfunding():
+    return render_template('crowd.html')
+
+
+@main.route('/platform')
+def platform():
+    return render_template('platform.html')
+
+
+@main.route('/what-we-need')
+def required():
+    return render_template('required.html')
+
 @main.route('/test')
 def test():
-    return render_template('required.html')
+    return render_template('base.html')
